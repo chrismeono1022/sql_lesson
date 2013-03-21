4ae89ab6ab1298c42faa0f6f54a45bcb1a509de0
Hackbright Student <user@mini-002.(none)>
2013-03-21 14:59:33 -0700

    connect sql with python

--- hackbright_app.py
+++ hackbright_app.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-DB = None
-CONN = None
+CONN = sqlite3.connect("my_database.db")
+DB = CONN.cursor()
 
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
@@ -11,9 +11,45 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values(?,?,?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+def get_project(title):
+    query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,)) 
+    row = DB.fetchone()
+    print """\
+Project: %s
+Description: %s
+Max grade: %s"""%(row[0], row[1], row[2])
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects values(?,?,?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added project: %s %s %s" %(title, description, max_grade) 
+
+def student_grade(project_title):
+    query = """SELECT student_github, project_title, grade FROM Grades WHERE project_title = ?"""
+    DB.execute(query, (project_title,))
+    row = DB.fetchone()
+    print """\
+Student Github: %s
+Project: %s
+Grade: %s"""%(row[0], row[1], row[2])
+
+def  grade_student(student_github, project_title, grade):
+    query = """INSERT into Grades values(?,?,?)"""
+    DB.execute(query, (student_github, project_title, grade))
+    CONN.commit()
+    print "Successfully graded student: %s %s %s" %(student_github, project_title, grade)
+
 def connect_to_db():
     global DB, CONN
-    CONN = sqlite3.connect("hackbright.db")
+    CONN = sqlite3.connect("my_database.db")
     DB = CONN.cursor()
 
 def main():
@@ -29,6 +65,18 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "projects":
+            new_args = [" ".join(args)]
+            get_project(*new_args)
+        elif command == "new_project":
+            new_args = " ".join(args).split(",")
+            make_new_project(*new_args)
+        elif command == "grade":
+            new_args = [" ".join(args)]
+            student_grade(*new_args)
+        elif command == "grade_student":
+            new_args = " ".join(args).split(",")
+            grade_student(*new_args)
 
     CONN.close()
 
